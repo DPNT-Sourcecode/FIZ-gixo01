623dfd8c00429e17e63092c87f7780666d9e9999
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 14:12:57 +0000

    Sat Aug 18 14:12:57 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,14 +1,26 @@
 package befaster.solutions.HLO;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 
 import org.junit.Test;
 
 public class HelloSolutionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void hello_hould_return_String_with_Hello_prefix() {
+	// arrange
+		HelloSolution helloSolution= new HelloSolution();
+		
+		// act
+		String response=helloSolution.hello("Mike");
+		
+		// assert
+		
+		assertThat(response,equalTo("Hello Mike"));
+		
 	}
 
 }
