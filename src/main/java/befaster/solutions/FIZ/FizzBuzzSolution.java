1c9e50dd38728468ca7035f33b5a7485890016e1
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 14:39:15 +0000

    Sat Aug 18 14:39:15 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,8 +6,13 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
     	if(number % 3==0)
+    	{
     		return "fizz";
-    	
+    	}
+    	if(number % 5==0)
+    	{
+    		return "buzz";
+    	}
     	else return Integer.toString(number);
     }
 
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,15 +1,16 @@
 package befaster.solutions.FIZ;
 
 
-import org.junit.Before;
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class FizzBuzzSolutionTest {
-	
+
 	private FizzBuzzSolution fizzBuzzSolution;
-	
+
 	@Before
 	public void setup()
 	{
@@ -18,24 +19,24 @@ public class FizzBuzzSolutionTest {
 
 	@Test
 	public void fizzBuzz_should_return_fizz_for3() {
-		
+
 		//act
 		String response=fizzBuzzSolution.fizzBuzz(6);
-		
+
 		//assert
 		assertThat(response,equalTo("fizz"));
-		
+
 	}
 
-	
+
 	@Test
 	public void fizzBuzz_should_return_buzz_for5() {
-		
+
 		//act
-				String response=fizzBuzzSolution.fizzBuzz(10);
-				
-				//assert
-				assertThat(response,equalTo("fizz"));
-	
+		String response=fizzBuzzSolution.fizzBuzz(10);
+
+		//assert
+		assertThat(response,equalTo("buzz"));
+
 	}
 }
