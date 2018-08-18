9773f9684edf57d53682df88d4b06cc82381e5a6
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 15:38:03 +0000

    Sat Aug 18 15:38:03 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,7 @@ public class FizzBuzzSolution {
 	public String fizzBuzz(Integer number) {
 
 
-		if(number % 3==0 && number % 5==0 ||(number % 3==0 &&  Integer.toString(number).contains("5") ) ||(number % 5==0 && Integer.toString(number).contains("3") ))
+		if(number % 3==0 && number % 5==0 ||(number % 3==0 &&  Integer.toString(number).contains("5") ) ||(number % 5==0 && Integer.toString(number).contains("3") ) ||( Integer.toString(number).contains("3")  && Integer.toString(number).contains("5") ) )
 		{
 			return "fizz buzz";
 		}
