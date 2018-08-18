4d71997a4ef057bd7de6bae2b181f08a4b768506
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 16:38:46 +0000

    Sat Aug 18 16:38:46 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -36,7 +36,7 @@ public class FizzBuzzSolutionTest {
 		String response=fizzBuzzSolution.fizzBuzz(666);
 
 		//assert
-		assertThat(response,equalTo("fizz delux"));
+		assertThat(response,equalTo("fizz deluxe"));
 
 	}
 	
@@ -70,7 +70,7 @@ public class FizzBuzzSolutionTest {
 		String response=fizzBuzzSolution.fizzBuzz(55);
 
 		//assert
-		assertThat(response,equalTo("buzz delux"));
+		assertThat(response,equalTo("buzz deluxe"));
 
 	}
 	
@@ -132,6 +132,6 @@ public class FizzBuzzSolutionTest {
 		String response=fizzBuzzSolution.fizzBuzz(77);
 
 		//assert
-		assertThat(response,equalTo("delux"));
+		assertThat(response,equalTo("deluxe"));
 	}
 }
