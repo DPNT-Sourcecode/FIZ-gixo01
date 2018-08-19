48132c5ee33ec39836e61ec607992b7d9f953100
sbx_user1061 <sbx_user1061@ip-10-129-203-44.eu-west-2.compute.internal>
2018-08-19 13:38:48 +0000

    Sun Aug 19 13:38:48 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,5 +1,7 @@
 package befaster.solutions.FIZ;
 
+import com.google.common.base.CharMatcher;
+
 public class FizzBuzzSolution {
 
 	public String fizzBuzz(Integer number) {
@@ -7,9 +9,9 @@ public class FizzBuzzSolution {
 
 		if(number % 3==0 && number % 5==0 ||(number % 3==0 &&  Integer.toString(number).contains("5") ) ||(number % 5==0 && Integer.toString(number).contains("3") ) ||( Integer.toString(number).contains("3")  && Integer.toString(number).contains("5") ) )
 		{
-
-			return (isDeluxNumber(number) ) ?((isFake(number) ? "fizz buzz fake deluxe" :"fizz buzz deluxe" ) ):"fizz buzz" ;
-
+			
+			return (isDeluxNumber(number) ) ?((isFake(number) ? "fizz buzz fake deluxe" :" fizz buzz  deluxe" ) ):"fizz buzz" ;
+			
 		}
 		if(number % 3==0 || Integer.toString(number).contains("3") )
 		{
@@ -17,7 +19,7 @@ public class FizzBuzzSolution {
 		}
 		if(number % 5==0 || Integer.toString(number).contains("5"))
 		{
-			return isDeluxNumber(number) ? ((isFake(number) ? "buzz fake deluxe" :"buzz deluxe" ) ):"buzz" ;
+			return isDeluxNumber(number) ? ((isFake(number) ? "buzz fake deluxe" :"buzz  deluxe" ) ):"buzz" ;
 		}
 		return  isDeluxNumber(number) ? ((isFake(number) ? "fake deluxe" :"deluxe" ) ) :Integer.toString(number);
 	}
@@ -25,16 +27,19 @@ public class FizzBuzzSolution {
 
 	private boolean isDeluxNumber(int number)
 	{
-		if(number % 3==0 && Integer.toString(number).contains("3") ) {
-			return true;
-		}
-		if(number % 5==0 && Integer.toString(number).contains("5") ) {
-			return true;
-		}
-		return false;
+		return (number > 10 && isIdentical(number)) ? true:false;
+	
 	}
 
 
+	private Boolean isIdentical(int number)
+	{
+		String stringNumber=Integer.toString(number);
+		int count=CharMatcher.is(stringNumber.charAt(0)).countIn(stringNumber);
+		return 	(count== stringNumber.length()) ? true : false;
+	}
+	
+	
 	private boolean isFake(int number)
 	{
 		return (number %2 != 0)? true:false;
